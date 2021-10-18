bb1b9abf3ad93c72ca05e8ef556909207dc14914
maharabhossain1 <maharabhossain0022@gmail.com>
2021-10-19 00:02:50 +0600

    Footer done

--- src/components/AboutUs/About.js
+++ src/components/AboutUs/About.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function About() {
   return (
     <div>
-      <h1>this is about us</h1>
+      <Container>
+        <Row xs={2} md={4} lg={6}>
+          <Col>1 of 2</Col>
+          <Col>2 of 2</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,52 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { FaFacebook, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
+import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div className="bg-dark p-4">
-      <div className="container bg-dark p-4 d-flex  justify-content-center">
+    <div className="bg-dark pt-5">
+      <Container>
+        <Row xs={1} md={4} lg={6} className="justify-content-evenly">
+          <Col>
+            <ul class="text-light">
+              <h4>Policy Info</h4>
+              <li>Editorial Policy</li>
+              <li>Privacy Policy</li>
+              <li>Vulnerability Disclosure Policy</li>
+              <li>Terms and Conditions</li>
+              <li>Customer Support Policy</li>
+              <li>Return Policy</li>
+            </ul>
+          </Col>
+          <Col>
+            <ul class="text-light">
+              <h4>Useful Links</h4>
+              <li>FAQ</li>
+              <li>About us</li>
+              <li>Contect us</li>
+              <li>Special Offers</li>
+              <li>Health Store</li>
+              <li>Need Help?</li>
+            </ul>
+          </Col>
+          <Col className="text-light">
+            <ul>
+              <h4>Social Medial </h4>
+              <li>Get in touched by Social media</li>
+              <li>
+                <FaFacebook class="fs-1 m-1" />
+                <FaGithub class="fs-1 m-1" />
+                <FaInstagram class="fs-1 m-1" />
+                <FaTwitter class="fs-1 m-1" />
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+      <div className="container bg-dark mt-3  d-flex  justify-content-center">
         <p className="text-xs-center text-light">
-          &copy;{new Date().getFullYear()} M.Madrasa -All Rights
+          &copy;{new Date().getFullYear()} Medi Sheba -All Rights
         </p>
       </div>
     </div>
